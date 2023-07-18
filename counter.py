7fada9e16368508ee022bdd2548af85481f490c2
iWatger <ethanfox646@icloud.com>
2023-07-17 22:40:33 -0400

    push

--- counter.py
+++ counter.py
@@ -1,13 +1,25 @@
-# This is my first part
 from sense_hat import SenseHat
+from time import sleep
 
 sense = SenseHat()
 
-sense.set_pixel(2, 2, (0, 0, 255))
-sense.set_pixel(4, 2, (0, 0, 255))
-sense.set_pixel(3, 4, (100, 0, 0))
-sense.set_pixel(1, 5, (255, 0, 0))
-sense.set_pixel(2, 6, (255, 0, 0))
-sense.set_pixel(3, 6, (255, 0, 0))
-sense.set_pixel(4, 6, (255, 0, 0))
-sense.set_pixel(5, 5, (255, 0, 0))
+w = (150, 150, 150)
+b = (0, 0, 255)
+e = (0, 0, 0)
+
+image = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+w,w,w,e,e,w,w,w,
+w,w,b,e,e,w,w,b,
+w,w,w,e,e,w,w,w,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e
+]
+
+sense.set_pixels(image)
+
+while True:
+    sleep(1)
+    sense.flip_h()
