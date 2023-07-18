f29d308fd3bfa4d0081643387b1b7d07fbe5245a
iWatger <ethanfox646@icloud.com>
2023-07-17 22:37:31 -0400

    this is a test

--- counter.py
+++ counter.py
@@ -1,3 +1,13 @@
 # This is my first part
+from sense_hat import SenseHat
 
-print("Hello, World!")
+sense = SenseHat()
+
+sense.set_pixel(2, 2, (0, 0, 255))
+sense.set_pixel(4, 2, (0, 0, 255))
+sense.set_pixel(3, 4, (100, 0, 0))
+sense.set_pixel(1, 5, (255, 0, 0))
+sense.set_pixel(2, 6, (255, 0, 0))
+sense.set_pixel(3, 6, (255, 0, 0))
+sense.set_pixel(4, 6, (255, 0, 0))
+sense.set_pixel(5, 5, (255, 0, 0))
