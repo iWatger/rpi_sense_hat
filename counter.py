724c3f47f359b5073f92bb280f54092095fc9140
iWatger <ethanfox646@icloud.com>
2023-07-29 00:13:57 -0400

    Hello

--- counter.py
+++ counter.py
@@ -2,27 +2,18 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-# Display the letter J
-sense.show_letter("E")
+X = [255, 0, 0]  # Red
+O = [255, 255, 255]  # White
 
-while True:
-	acceleration = sense.get_accelerometer_raw()
-	x = acceleration['x']
-	y = acceleration['y']
-	z = acceleration['z']
+question_mark = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+]
 
-	x=round(x, 0)
-	y=round(y, 0)
-	z=round(z, 0)
-	
-	print("x={0}, y={1}, z={2}".format(x, y, z))
-
-  # Update the rotation of the display depending on which way up the Sense HAT is
-	if x  == -1:
-	  sense.set_rotation(180)
-	elif y == 1:
-	  sense.set_rotation(90)
-	elif y == -1:
-	  sense.set_rotation(270)
-	else:
-	  sense.set_rotation(0)
+sense.set_pixels(question_mark)
